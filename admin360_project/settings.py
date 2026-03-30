d4e16f120ee9da7068c51ad32a32b38e2157d489
Yas <yasmairayruizd@gmail.com>
2026-03-29 23:51:39 -0300

    Fix: Datos

--- admin360_project/settings.py
+++ admin360_project/settings.py
@@ -2,10 +2,10 @@
 import os
 # Configuración de email para producción en Railway usando variables en español
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('HOST DE CORREO ELECTRÓNICO')
-EMAIL_PORT = int(os.environ.get('PUERTO DE CORREO ELECTRÓNICO', 587))
-EMAIL_HOST_USER = os.environ.get('USUARIO_DEL_HOST_DE_CORREO_ELECTRÓNICO')
-EMAIL_HOST_PASSWORD = os.environ.get('CONTRASEÑA_DEL_HOST_DE_CORREO_ELECTRÓNICO')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Railway: Permitir el dominio público para CSRF
