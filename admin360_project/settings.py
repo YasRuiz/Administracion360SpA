baf7702c0a56f4d0d30b59a9bfd7793de62b4352
Yas <yasmairayruizd@gmail.com>
2026-03-29 23:30:50 -0300

    Configura email SMTP con variables de entorno para producción

--- admin360_project/settings.py
+++ admin360_project/settings.py
@@ -1,3 +1,11 @@
+# Configuración de email para producción en Railway
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Railway: Permitir el dominio público para CSRF
 CSRF_TRUSTED_ORIGINS = ['https://soothing-victory-production.up.railway.app']
 """
